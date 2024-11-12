79d350935575ed21844b53937e0bb25b0565ea0c
LennyBrajcic <200574671@student.georgianc.on.ca>
2024-11-12 11:27:22 -0500

    fixed errors

--- js/json.js
+++ js/json.js
@@ -6,7 +6,7 @@ const section = document.querySelector('section');
 async function populate(){
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    const requestURL = './js/i-scream.json'
+    const requestURL = '/COMP1073F-Lesson-10-Lab-4-/js/i-scream.json'
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
@@ -49,7 +49,7 @@ function showTopFlavors(jsonObj) {
 
         // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
         h2.textContent = topFlavors[i]['name'];
-        image.setAttribute('src', `./images/${topFlavors[i].image}`);
+        image.setAttribute('src', `/COMP1073F-Lesson-10-Lab-4-/images/${topFlavors[i].image}`);
 
         // STEP 10g: Build a loop for the ingredients array in the JSON
         let ingredients = topFlavors[i]['ingredients'];
